7b2bec0dcd82f057560c5ae08ff63bcf72e66f46
abbirku <abbir.ku@gmail.com>
2021-08-22 22:52:08 +0600

    Create file and file info adapter

---
+++ CSharpLibraryTools/CoreActivities/FileManager/FileInfoAdapter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CoreActivities.FileManager
+{
+    public class FileInfoAdapter : IFileInfo
+    {
+        private FileInfo ObjectCreationAndValidation(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+                throw new Exception("Provide valid file path");
+
+            var fileInfo = new FileInfo(filePath);
+            if (!fileInfo.Exists)
+                throw new Exception("File does not exists");
+
+            return fileInfo;
+        }
+
+        public DateTime CreatedOn(string filePath) => ObjectCreationAndValidation(filePath).CreationTime;
+
+        public long FileSize(string filePath) => ObjectCreationAndValidation(filePath).Length;
+
+        public bool IsReadOnly(string filePath) => ObjectCreationAndValidation(filePath).IsReadOnly;
+
+        public DateTime LastAccessOn(string filePath) => ObjectCreationAndValidation(filePath).LastAccessTime;
+
+        public DateTime LastUpdateOn(string filePath) => ObjectCreationAndValidation(filePath).LastWriteTime;
+    }
+}
---
+++ CSharpLibraryTools/CoreActivities/FileManager/IFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreActivities.FileManager
+{
+    public interface IFile
+    {
+        string FileName(string filePath);
+        bool DoesExists(string filePath);
+        void CreateFile(string filePath);
+        string GetMimeType(string fileName);
+        Task WriteAllLineAsync(List<string> lines, string filePath);
+        Task WriteAllTextAsync(string text, string filePath);
+        Task AppendAllLineAsync(List<string> lines, string filePath);
+        Task AppendAllTextAsync(string text, string filePath);
+        Task<string[]> ReadAllLineAsync(string filePath);
+        Task<string> ReadAllTextAsync(string filePath);
+    }
+}
