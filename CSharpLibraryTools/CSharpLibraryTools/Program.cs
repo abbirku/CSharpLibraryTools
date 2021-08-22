a6adb4440063be592de73ea12c4da05984fc63b9
abbirku <abbir.ku@gmail.com>
2021-08-22 18:40:07 +0600

    Provide package for ActiveProgram to resolve DI overhead

--- CSharpLibraryTools/CSharpLibraryTools/Program.cs
+++ CSharpLibraryTools/CSharpLibraryTools/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Autofac;
+using CoreActivities.ActiveProgram;
+using System;
 
 namespace CSharpLibraryTools
 {
     class Program
     {
+        /// <summary>
+        /// Need to only inject the packages inside CompositionRoot
+        /// </summary>
+        private static IContainer CompositionRoot()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Application>();
+            builder.RegisterModule(new ActiveProgramPackage());
+
+            return builder.Build();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CompositionRoot().Resolve<Application>().Run();
         }
     }
 }
