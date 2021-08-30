78b90242b53a5905ad1ea4c65cae234c16b77914
abbirku <abbir.ku@gmail.com>
2021-08-30 23:21:19 +0600

    Create BrowserActivityImp

---
+++ CSharpLibraryTools/CSharpLibraryTools/Implementations/BrowseActivityImp.cs
@@ -0,0 +1,25 @@
+﻿using CoreActivities.BrowserActivity;
+using System;
+
+namespace CSharpLibraryTools
+{
+    public class BrowseActivityImp
+    {
+        private readonly IBrowserActivity _browserActivity;
+
+        public BrowseActivityImp(IBrowserActivity browserActivity)
+        {
+            _browserActivity = browserActivity;
+        }
+
+        public void Run()
+        {
+            var activeUrl = _browserActivity.EnlistActiveTabUrl(BrowserType.Chrome);
+            Console.WriteLine($"Active URL: {activeUrl}");
+
+            var tabs = _browserActivity.EnlistAllOpenTabs(BrowserType.Chrome);
+            foreach (var item in tabs)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ CSharpLibraryTools/CSharpLibraryTools/Implementations/DirectoryManagerImg.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpLibraryTools.Implementations
+{
+    class DirectoryManagerImg
+    {
+    }
+}
