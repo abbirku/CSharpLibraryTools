455904d6d06937b4288da2b7bd011d99a982cd6c
abbirku <abbir.ku@gmail.com>
2021-08-30 22:46:49 +0600

    Create active program implementation

---
+++ CSharpLibraryTools/CSharpLibraryTools/ActiveProgramImp.cs
@@ -0,0 +1,20 @@
+﻿using CoreActivities.ActiveProgram;
+using System;
+
+namespace CSharpLibraryTools
+{
+    public class ActiveProgramImp
+    {
+        private readonly IActiveProgram _activeProgram;
+
+        public ActiveProgramImp(IActiveProgram activeProgram)
+            => _activeProgram = activeProgram;
+
+        public void Run()
+        {
+            Console.WriteLine($"Active Program: {_activeProgram.CaptureActiveProgramTitle()}");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
